5e5bdcacf9bbc08057b9d993b10c4f5c5a673f64
rere <regis-graptin@sfr.fr>
2025-02-09 15:21:50 +0700

    feat: update wagmi config

--- front/src/utils/wagmi.ts
+++ front/src/utils/wagmi.ts
@@ -1,23 +1,15 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  scroll,
+  scrollSepolia,
 } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: "RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  appName: process.env.NEXT_PUBLIC_SITE_NAME!,
+  projectId: process.env.REOWN_PROJECT_ID!,
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+    scroll,
+    ...(process.env.DEV_ENV === "true" ? [scrollSepolia] : []),
   ],
   ssr: true,
 });
